86b42e6aed6c75b7708d067f7ccb2af7ee6ddc52
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-29 11:03:24 -0300

    feat: creating a context processor for the current cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-'cart.apps.CartConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +56,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
