53e8239f5943b3af59970b62ae9b4c29349e65d9
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-29 15:00:17 -0300

    feat: monitoring celery with flower

--- myshop/settings.py
+++ myshop/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
