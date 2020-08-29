bb60938a06e63e1efd9aa0e32a3235d69d78302c
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-29 10:38:47 -0300

    feat: creating shopping cart views

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
- 
- 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
